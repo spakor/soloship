ab09d346204f124d1d3098491f95fbb96b2cc19e
Seongwon Han <seongwon@seongwonhan.com>
2024-07-09 11:02:29 -0400

    context updated.

--- solopreunership/context.py
+++ solopreunership/context.py
@@ -25,54 +25,70 @@ with open("solopreunership/business_examples.json", "r") as f:
 random_sample_niche_examples = ", ".join([example_dict["niches"][0] for example_dict in random.sample(examples, 5)])
 random_sample_business_examples = ", ".join([example_dict["business"] for example_dict in random.sample(examples, 5)])
 
-TIME_CONSIERATION = """Ensure to include the time estimation for building audience. State the assumptions if made. Provide detailed reasoning for estimation."""
-COST_CONSIERATION = """Ensure to include the operation cost estimation(in USD) in the first year month by month. Provide detailed reasoning for estimation."""
+TIME_CONSIERATION = """Ensure to include the time estimation for building audience. State the assumptions if made. 
+                        Provide detailed reasoning for estimation in bulletpoints."""
+COST_CONSIERATION = """Ensure to include the operation cost estimation(in USD) in the first year month by month. 
+                        Provide detailed reasoning for estimation in bulletpoints."""
 
 PROMPT_1 = (
     "## Summary", 
-    """Provide a quick summary and breakdown of my professional experience and myself in 200 words or less."""
+    """
+        Provide a quick summary of your understanding of user's experience and desires in bulletpoints up to 5.
+        Exact word count: [50 to 100]
+        Always address the user as "You", and do not include the user's name.
+    """
 )
 PROMPT_2 = (
     "## Niche Recommendations",
-    f"""Provide top 5 niche recommendations related to the user's current skills and interests. It's better for the recommendations to encompass diverse fields, not constrained to particular field. (e.g. {random_sample_niche_examples})
-DO THIS ONLY IF THE USER DID NOT INPUT ANY BUSINESS IDEA.
-"""
-    + TIME_CONSIERATION + " Let's think step-by-step."
+    f"""
+        Provide top 5 niche recommendations related to the user's current skills and interests. 
+        It's better for the recommendations to encompass diverse fields, not constrained to particular field. (e.g. {random_sample_niche_examples})
+        Let's think step-by-step, but do not display each step in response.
+        Always address the user as "You", and do not include the user's name.
+    """
 )
 PROMPT_3 = (
     "## Business Recommendations",
-    f"""Provide top 5 one-person business recommendations related to the user's current skills and interests. It's better for the recommendations to encompass diverse fields, not constrained to particular field. (e.g. {random_sample_business_examples})
-BUSINESS RECOMMENDATIONS SHOULD ENCAPTULATE THE NICHE(IF PROVIDED), BUT NOT IDENTICAL. 
-"""
-    + TIME_CONSIERATION + " Let's think step-by-step."
+    f"""
+        Provide top 5 one-person business recommendations related to the user's professional experience and personal interests. 
+        It's better for the recommendations to encompass diverse fields, not constrained to particular field. (e.g. {random_sample_business_examples})
+        BUSINESS MODELS SHOULD ENCAPTULATE THE RECOMMENDED NICHES, NOT IDENTICAL. 
+        Consider the time and cost tolerance from the user's input when recommending a business. 
+        Ensure the recommended businesses are small but scalable over time. 
+        {TIME_CONSIERATION}
+        Let's think step-by-step.
+        Always address the user as "You", and do not include the user's name.
+    """
 )
 PROMPT_4 = (
     "## Required business skills and knowledge",
     """
-Provide a list of skills and knowledge for recommended businesses. 
-Include every aspect of business, such as marketing, accounting, legal, etc.
-"""
-    + COST_CONSIERATION + " Let's think step-by-step."
+        Provide a list of skills and knowledge for recommended businesses. 
+        Include every aspect of business, such as marketing, accounting, legal, etc.
+        The response must be in a formatted table.
+        Ensure to inlcude online resources(learning platforms) to acquire each skill and knowledge
+        Let's think step-by-step.
+        Always address the user as "You", and do not include the user's name.
+    """
 )
 PROMPT_5 = (
     "## Services and Tools needed",
     """
-Provide a list of services and tools required to operate recommended businesses to success. 
-These may include emailing, researching, marketing, etc. 
-"""
-    + COST_CONSIERATION + " Let's think step-by-step."
+        Provide a list of existing services and tools required to operate recommended businesses to success. 
+        These may include emailing, researching, marketing, etc. 
+        For each service and tool, you must provide a service provider name. ie. email MailChimp
+        The response must be in a formatted table.
+        {COST_CONSIERATION}
+        Let's think step-by-step.
+        Always address the user as "You", and do not include the user's name.
+    """
 )
 
 SYSTEM_CONTEXT = (
     """
-    You are an experienced business consultant who is going to help the user to start a one-person business based on the user input. 
-    You must not assume or add anything that was not mentioned. 
-"""
-    # + PROMPT_1
-    # + PROMPT_2
-    # + PROMPT_3
-    # + PROMPT_4
-    # + PROMPT_5
+        You are an experienced business consultant who is going to help the user to start a one-person business based on the user input. 
+        You must not assume or add anything that was not mentioned. 
+    """
 )
 
 
